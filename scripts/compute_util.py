d1fc73587a41ffac0d95222f2e758a22738a9cb1
Midhul <midhul.v@gmail.com>
2021-05-05 12:26:02 -0400

    Fairness computation

--- scripts/compute_util.py
+++ scripts/compute_util.py
@@ -14,6 +14,13 @@ def compute_utilization(allocations, raw_demands, total_blocks):
     util = float(used_capacity)/float(total_blocks * num_epochs)
     return util
 
+def compute_fairness(allocations):
+    sum_allocs = []
+    for t in allocations:
+        sum_allocs.append(sum(allocations[t]))
+
+    return float(min(sum_allocs))/float(max(sum_allocs))
+
 config = sys.argv[1]
 trace_file = sys.argv[2]
 fair_share = int(sys.argv[3])
@@ -28,3 +35,6 @@ raw_demands = get_demands(trace_file, fair_share)
 
 print('Utilization')
 print(compute_utilization(allocs, raw_demands, total_blocks))
+
+print('Fairness (min/max)')
+print(compute_fairness(allocs))
