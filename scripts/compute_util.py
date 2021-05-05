78580864af317b1917ebdaa494fea59b0e1f16de
Midhul <midhul.v@gmail.com>
2021-05-05 02:07:35 -0400

    Set file limit

---
+++ scripts/compute_util.py
@@ -0,0 +1,30 @@
+import os, pickle, sys
+
+from karma_sim.util import *
+from karma_sim.allocator import Allocator,MaxMinAllocator,StaticAllocator
+
+def compute_utilization(allocations, raw_demands, total_blocks):
+    num_epochs = len(allocations[list(allocations.keys())[0]])
+    used_capacity = 0
+    for t in allocations:
+        used = []
+        for e in range(num_epochs):
+            used.append(min(allocations[t][e], raw_demands[t][e]))
+        used_capacity += sum(used)
+    util = float(used_capacity)/float(total_blocks * num_epochs)
+    return util
+
+config = sys.argv[1]
+trace_file = sys.argv[2]
+fair_share = int(sys.argv[3])
+total_blocks = int(sys.argv[4])
+
+prefix = '/home/ubuntu/karma-eval/'
+alloc_file = prefix + config + '.alloc'
+with open(alloc_file, 'rb') as handle:
+    allocs = pickle.load(handle)
+
+raw_demands = get_demands(trace_file, fair_share)
+
+print('Utilization')
+print(compute_utilization(allocs, raw_demands, total_blocks))
