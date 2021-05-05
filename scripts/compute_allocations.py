a53ce80b7e37544cdd535f0c06ae77f605d65ba7
Midhul <midhul.v@gmail.com>
2021-05-05 12:01:42 -0400

    updates for fairness part

--- scripts/compute_allocations.py
+++ scripts/compute_allocations.py
@@ -1,4 +1,4 @@
-# python3 compute_allocations.py test /home/midhul/snowflake_demands_nogaps10.pickle static 100 90000 0
+# python3 compute_allocations.py test /home/midhul/snowflake_demands_nogaps10.pickle static 100 90000 0 100 0
 
 import os, pickle, sys
 
@@ -19,26 +19,33 @@ def smoothed_avg(a, alpha, avg):
 config = sys.argv[1]
 trace_file = sys.argv[2]
 alloc_type = sys.argv[3]
-fair_share = int(sys.argv[4])
+average = int(sys.argv[4])
 init_credits = int(sys.argv[5])
 public_blocks = int(sys.argv[6])
+guarantee = int(sys.argv[7])
+oracle = bool(sys.argv[8])
 
 prefix = '/home/ubuntu/karma-eval/' + config
 
-raw_demands = get_demands(trace_file, fair_share)
+raw_demands = get_demands(trace_file, average)
 
 demands = {}
-for t in raw_demands:
-    demands[t] = smoothed_avg(raw_demands[t], 0.5, fair_share)
+if oracle:
+    for t in raw_demands:
+        demands[t] = raw_demands[t][:]
+else:
+    for t in raw_demands:
+        demands[t] = smoothed_avg(raw_demands[t], 0.5, average)
 
-# Simulate reclaim
-for t in demands:
-    for i in range(len(demands[t])):
-        if demands[t][i] < fair_share and demands[t][i] < raw_demands[t][i]:
-            demands[t][i] = min(raw_demands[t][i], fair_share)
+    # Simulate reclaim
+    for t in demands:
+        for i in range(len(demands[t])):
+            if demands[t][i] < guarantee and demands[t][i] < raw_demands[t][i]:
+                demands[t][i] = min(raw_demands[t][i], guarantee)
 
 num_tenants = len(demands)
-capacity = num_tenants*fair_share
+capacity = num_tenants*average
+taken_away = num_tenants*(average-guarantee)
 
 alloc = None
 if alloc_type == 'static':
@@ -46,7 +53,7 @@ if alloc_type == 'static':
 elif alloc_type == 'maxmin':
     alloc = MaxMinAllocator(raw_demands, total_blocks=capacity)
 elif alloc_type == 'karma':
-    alloc = Allocator(demands, total_blocks=capacity+public_blocks, init_credits=init_credits, public_blocks=public_blocks)
+    alloc = Allocator(demands, total_blocks=capacity+public_blocks, init_credits=init_credits, public_blocks=taken_away + public_blocks)
 else:
     raise Exception('unsupported allocator')
 
