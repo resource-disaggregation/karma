bcef3ae5e2d627ab024134b5127d3858cba6cb30
Midhul <midhul.v@gmail.com>
2021-05-05 20:37:29 -0400

    x

--- scripts/compute_util.py
+++ scripts/compute_util.py
@@ -21,6 +21,24 @@ def compute_fairness(allocations):
 
     return float(min(sum_allocs))/float(max(sum_allocs))
 
+def compute_jiffy_blocks(allocations, raw_demands):
+    num_epochs = len(allocations[list(allocations.keys())[0]])
+    used_capacity = 0
+    for t in allocations:
+        used = []
+        for e in range(num_epochs):
+            used.append(min(allocations[t][e], raw_demands[t][e]))
+        used_capacity += sum(used)
+    return used
+
+def compute_s3_blocks(allocations, raw_demands):
+    total_blocks = 0
+    for t in raw_demands:
+        total_blocks += sum(raw_demands[t])
+    
+    return total_blocks - compute_jiffy_blocks(allocations, raw_demands)
+
+
 config = sys.argv[1]
 trace_file = sys.argv[2]
 fair_share = int(sys.argv[3])
@@ -38,3 +56,9 @@ print(compute_utilization(allocs, raw_demands, total_blocks))
 
 print('Fairness (min/max)')
 print(compute_fairness(allocs))
+
+print('Jiffy blocks')
+print(compute_jiffy_blocks(allocs, raw_demands))
+
+print('S3 blocks')
+print(compute_s3_blocks(allocs, raw_demands))
