1d7b1fdc8d9122353368d18bcd15b59d4c0b35f5
Midhul <midhul.v@gmail.com>
2021-09-04 17:43:35 -0400

    no run mode

--- scripts/run_drivers.sh
+++ scripts/run_drivers.sh
@@ -26,6 +26,7 @@ public_blocks="${14}"
 guarantee="${15}"
 estimator="${16}"
 error="${17}"
+mode="${18}"
 # tenant_ids=('92' '45' '14' '79' '93' '1' '54' '96' '44' '76')
 # tenant_ids=('0' '1' '2' '3' '4' '5' '6' '7' '8' '9' '10' '11' '12' '13' '14' '15' '16' '17' '18' '19' '20' '21' '22' '23' '24' '25' '26' '27' '28' '29' '30' '31' '32' '33' '34' '35' '36' '37' '38' '39' '40' '41' '42' '43' '44' '45' '46' '47' '48' '49' '50' '51' '52' '53' '54' '55' '56' '57' '58' '59' '60' '61' '62' '63' '64' '65' '66' '67' '68' '69' '70' '71' '72' '73' '74' '75' '76' '77' '78' '79' '80' '81' '82' '83' '84' '85' '86' '87' '88' '89' '90' '91' '92' '93' '94' '95' '96' '97' '98' '99')
 # alt_tenants=('50' '51' '52' '53' '54' '55' '56' '57' '58' '59' '60' '61' '62' '63' '64' '65' '66' '67' '68' '69' '70' '71' '72' '73' '74' '75' '76' '77' '78' '79' '80' '81' '82' '83' '84' '85' '86' '87' '88' '89' '90' '91' '92' '93' '94' '95' '96' '97' '98' '99')
@@ -34,31 +35,35 @@ error="${17}"
 python3 compute_allocations.py $config $trace_file $alloc 100 $init_credits $public_blocks $guarantee $oracle $estimator $error $alt_file $selfish_file
 echo "Allocations computed"
 
-mapfile -t alt_tenants < $alt_file;
-mapfile -t selfish_tenants < $selfish_file;
+if [ "$mode" != "norun" ]; then
 
-pids=()
+    mapfile -t alt_tenants < $alt_file;
+    mapfile -t selfish_tenants < $selfish_file;
 
-echo "Starting altruistic tenants"
-for tenant in "${alt_tenants[@]}"
-do
-    python3 -u driver.py $dir_host $dir_porta $dir_portb $tenant $num_threads $trace_file $block_size $backing_path $oracle 0 "/home/ubuntu/karma-eval/$config.alloc" > ~/karma-eval/$config.tenant$tenant.txt 2>&1 &
-    pids+=($!);
-    echo "Launched tenant$tenant";
-done
+    pids=()
 
-echo "---------"
-echo "Starting selfish tenants"
+    echo "Starting altruistic tenants"
+    for tenant in "${alt_tenants[@]}"
+    do
+        python3 -u driver.py $dir_host $dir_porta $dir_portb $tenant $num_threads $trace_file $block_size $backing_path $oracle 0 "/home/ubuntu/karma-eval/$config.alloc" > ~/karma-eval/$config.tenant$tenant.txt 2>&1 &
+        pids+=($!);
+        echo "Launched tenant$tenant";
+    done
 
-for tenant in "${selfish_tenants[@]}"
-do
-    python3 -u driver.py $dir_host $dir_porta $dir_portb $tenant $num_threads $trace_file $block_size $backing_path $oracle 1 "/home/ubuntu/karma-eval/$config.alloc" > ~/karma-eval/$config.tenant$tenant.txt 2>&1 &
-    pids+=($!);
-    echo "Launched tenant$tenant";
-done
+    echo "---------"
+    echo "Starting selfish tenants"
 
-for pid in ${pids[*]}; do
-    wait $pid
-done
+    for tenant in "${selfish_tenants[@]}"
+    do
+        python3 -u driver.py $dir_host $dir_porta $dir_portb $tenant $num_threads $trace_file $block_size $backing_path $oracle 1 "/home/ubuntu/karma-eval/$config.alloc" > ~/karma-eval/$config.tenant$tenant.txt 2>&1 &
+        pids+=($!);
+        echo "Launched tenant$tenant";
+    done
+
+    for pid in ${pids[*]}; do
+        wait $pid
+    done
+    
+fi
 
 echo "Done"
