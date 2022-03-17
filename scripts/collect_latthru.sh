6c8581ee232ce861873322550f92a7f9320deb29
Midhul <midhul.v@gmail.com>
2022-03-17 18:49:48 -0400

    latency throughput curves

--- scripts/collect_latthru.sh
+++ scripts/collect_latthru.sh
@@ -2,12 +2,14 @@ cfg=$1
 
 for para in 1 2 4 8 16; do
     prefix="~/karma-eval/$cfg-latthru-para$para";
-    lat_sum=$(cat $prefix.tenant* | grep -i 'latency sum' | awk '{sum += $5;} END {print sum}')
-    lat_count=$(cat $prefix.tenant* | grep -i 'latency count' | awk '{sum += $5;} END {print sum}')
-    jiffy_blocks=$(cat $prefix.tenant* | grep -i 'jiffy blocks' | awk '{sum += $5} END {print sum}')
-    persistent_blocks=$(cat $prefix.tenant* | grep -i 'persistent blocks' | awk '{sum += $5} END {print sum}')
-    total_ops=$(cat $prefix.tenant* | grep -i 'total ops' | awk '{sum += $5} END {print sum}')
-    duration=$(cat $prefix.tenant*.txt | grep -i 'execution time' | head | awk '{sum += $5} END {print sum}')
+    rm -f $prefix.dump.txt
+    cat $prefix.tenant* | grep -i -e '\[selfish\]' -e '\[alt\]' > $prefix.dump.txt
+    lat_sum=$(cat $prefix.dump.txt | grep -i 'latency sum' | awk '{sum += $5;} END {print sum}')
+    lat_count=$(cat $prefix.dump.txt | grep -i 'latency count' | awk '{sum += $5;} END {print sum}')
+    jiffy_blocks=$(cat $prefix.dump.txt | grep -i 'jiffy blocks' | awk '{sum += $5} END {print sum}')
+    persistent_blocks=$(cat $prefix.dump.txt | grep -i 'persistent blocks' | awk '{sum += $5} END {print sum}')
+    total_ops=$(cat $prefix.dump.txt | grep -i 'total ops' | awk '{sum += $5} END {print sum}')
+    duration=$(cat $prefix.dump.txt | grep -i 'execution time' | head | awk '{sum += $5} END {print sum}')
 
     avg_latency=$(echo - | awk -v sum=$lat_sum -v count=$lat_count '{print sum/count;}')
     xput=$(echo - | awk -v sum=$total_ops -v count=$duration '{print sum/count;}')
