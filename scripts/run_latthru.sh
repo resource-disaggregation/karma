1199990b354eed3fcbc4ed97549bc225acbe8a39
Midhul <midhul.v@gmail.com>
2022-03-17 04:50:00 -0400

    latency throughput curves

--- scripts/run_latthru.sh
+++ scripts/run_latthru.sh
@@ -21,13 +21,13 @@ echo "Starting tenants"
 for para in 1 2 4 8 16; do
     config="latthru-para$para";
     echo $config;
+    pids=()
     for ((tenant=0;tenant<$num_tenants;tenant++)); do
         python3 -u driver2.py $dir_host $dir_porta $dir_portb $tenant $para ~/karma-eval/microbench_demands.pickle $block_size foobar 0 0 ~/karma-eval/microbench_allocs.pickle $fair_share > ~/karma-eval/$config.tenant$tenant.txt 2>&1 &
         pids+=($!);
         echo "Launched tenant$tenant";
     done
-done
-
-for pid in ${pids[*]}; do
-    wait $pid
+    for pid in ${pids[*]}; do
+        wait $pid
+    done
 done
