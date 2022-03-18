44b94370d6c0929cc13941acc272982592ef0094
Midhul <midhul.v@gmail.com>
2022-03-18 18:49:23 -0400

    setup script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
 sudo apt update
-sudo apt install -y build-essential cmake python3-setuptools openjdk-8-jre openjdk-8-jdk maven
+sudo apt install -y build-essential cmake python3-setuptools python3-pip python-pip openjdk-8-jre openjdk-8-jdk maven
+
+python3 -m pip install thrift
+sudo python -m pip install thrift
+python3 -m pip install 'pytest-runner==3.0'
+sudo python -m pip install 'pytest-runner==3.0'
 
 mkdir -p build
 cd build
-cmake .. && make -j
+cmake .. && make -j && cd pyjiffy/ && python3 -m pip install .
