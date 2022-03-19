413b6db816b77ae1b8db9092d47673238f8a5d45
Midhul <midhul.v@gmail.com>
2022-03-18 23:55:14 -0400

    run_drivers2 sharding

--- scripts/run_drivers2.sh
+++ scripts/run_drivers2.sh
@@ -28,6 +28,8 @@ estimator="${16}"
 error="${17}"
 mode="${18}"
 capacity=100
+num_shards=$8
+shard_idx=$9
 # tenant_ids=('92' '45' '14' '79' '93' '1' '54' '96' '44' '76')
 # tenant_ids=('0' '1' '2' '3' '4' '5' '6' '7' '8' '9' '10' '11' '12' '13' '14' '15' '16' '17' '18' '19' '20' '21' '22' '23' '24' '25' '26' '27' '28' '29' '30' '31' '32' '33' '34' '35' '36' '37' '38' '39' '40' '41' '42' '43' '44' '45' '46' '47' '48' '49' '50' '51' '52' '53' '54' '55' '56' '57' '58' '59' '60' '61' '62' '63' '64' '65' '66' '67' '68' '69' '70' '71' '72' '73' '74' '75' '76' '77' '78' '79' '80' '81' '82' '83' '84' '85' '86' '87' '88' '89' '90' '91' '92' '93' '94' '95' '96' '97' '98' '99')
 # alt_tenants=('50' '51' '52' '53' '54' '55' '56' '57' '58' '59' '60' '61' '62' '63' '64' '65' '66' '67' '68' '69' '70' '71' '72' '73' '74' '75' '76' '77' '78' '79' '80' '81' '82' '83' '84' '85' '86' '87' '88' '89' '90' '91' '92' '93' '94' '95' '96' '97' '98' '99')
@@ -46,6 +48,9 @@ if [ "$mode" != "norun" ]; then
     echo "Starting altruistic tenants"
     for tenant in "${alt_tenants[@]}"
     do
+        if [[ $(($tenant%$num_shards)) -ne $shard_idx ]]; then
+            continue;
+        fi
         python3 -u driver2.py $dir_host $dir_porta $dir_portb $tenant $num_threads $trace_file $block_size $backing_path $oracle 0 ~/karma-eval/"$config.alloc" $capacity > ~/karma-eval/$config.tenant$tenant.txt 2>&1 &
         pids+=($!);
         echo "Launched tenant$tenant";
@@ -56,6 +61,9 @@ if [ "$mode" != "norun" ]; then
 
     for tenant in "${selfish_tenants[@]}"
     do
+        if [[ $(($tenant%$num_shards)) -ne $shard_idx ]]; then
+            continue;
+        fi
         python3 -u driver2.py $dir_host $dir_porta $dir_portb $tenant $num_threads $trace_file $block_size $backing_path $oracle 1 ~/karma-eval/"$config.alloc" $capacity > ~/karma-eval/$config.tenant$tenant.txt 2>&1 &
         pids+=($!);
         echo "Launched tenant$tenant";
