fe2e6599da62b607d34e7663ec1217eb4a25e6c5
Midhul <midhul.v@gmail.com>
2022-03-26 22:40:14 -0400

    sbin scripts

--- sbin/copy_bins.sh
+++ sbin/copy_bins.sh
@@ -7,4 +7,5 @@ mkdir -p /home/ubunu/deploy
 
 cp $sbin/../build/storage/storaged /home/ubunu/deploy/
 cp $sbin/../build/directory/directoryd /home/ubunu/deploy/
+cp $sbin/../scripts/driver4.py /home/ubunu/deploy/
 cp $sbin/* /home/ubunu/deploy/
