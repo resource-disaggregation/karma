2a7c4224196711a6e0f583cf6eeed2888cc87b75
Midhul <midhul.v@gmail.com>
2022-03-27 14:48:17 -0400

    sbin scripts

--- sbin/collect_throughput.sh
+++ sbin/collect_throughput.sh
@@ -6,9 +6,4 @@ sbin="`cd "$sbin"; pwd`"
 config=$1
 num_users=$2
 
-numbers=$(for ((i = 1 ; i <= $num_users ; i++)); do echo $i; done)
-static=$(cat ~/karma-eval/$config-static.results | grep -i throughput | awk '{print $5}' | sort -n)
-maxmin=$(cat ~/karma-eval/$config-maxmin.results | grep -i throughput | awk '{print $5}' | sort -n)
-karma=$(cat ~/karma-eval/$config-karma.results | grep -i throughput | awk '{print $5}' | sort -n)
-
 paste <(for ((i = 1 ; i <= $num_users ; i++)); do echo $i; done) <(cat ~/karma-eval/$config-static.results | grep -i throughput | awk '{print $5}' | sort -n) <(cat ~/karma-eval/$config-maxmin.results | grep -i throughput | awk '{print $5}' | sort -n) <(cat ~/karma-eval/$config-karma.results | grep -i throughput | awk '{print $5}' | sort -n)
