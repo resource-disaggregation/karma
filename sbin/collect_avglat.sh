21d7cc05b1d7a9933838789e1eed757874991cbd
Midhul <midhul.v@gmail.com>
2022-03-27 14:52:20 -0400

    sbin scripts

--- sbin/collect_avglat.sh
+++ sbin/collect_avglat.sh
@@ -6,4 +6,4 @@ sbin="`cd "$sbin"; pwd`"
 config=$1
 num_users=$2
 
-paste <(for ((i = 1 ; i <= $num_users ; i++)); do echo $i; done) <(cat ~/karma-eval/$config-static.results | grep -i "average latency" | awk '{print $6}' | sort -n) <(cat ~/karma-eval/$config-maxmin.results | grep -i "average latency" | awk '{print $6}' | sort -n) <(cat ~/karma-eval/$config-karma.results | grep -i "average latency" | awk '{print $6}' | sort -n)
+paste <(for ((i = 1 ; i <= $num_users ; i++)); do echo $i; done) <(cat ~/karma-eval/$config-static.results | grep -i "average latency" | awk '{print $6}' | sort -nr) <(cat ~/karma-eval/$config-maxmin.results | grep -i "average latency" | awk '{print $6}' | sort -nr) <(cat ~/karma-eval/$config-karma.results | grep -i "average latency" | awk '{print $6}' | sort -nr)
