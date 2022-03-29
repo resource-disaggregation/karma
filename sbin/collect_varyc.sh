d01890a3d7be4bdc22667cfc2529f019fbdb3d32
Midhul <midhul.v@gmail.com>
2022-03-29 00:35:13 -0400

    collect vary c

---
+++ sbin/collect_varyc.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+sbin="`dirname "$0"`"
+sbin="`cd "$sbin"; pwd`"
+
+config=$1
+total_blocks=$2
+duration=$3
+
+echo "Utilization"
+for c in 9 90 900 9000 90000 900000 9000000; do
+    cat ~/karma-eval/$config-c$c.results | grep -i "total useful allocation" | awk -v blocks=$total_blocks -v dur=$duration '{sum += $7} END {print sum/(blocks*dur);}'
+done
+
+echo "Throughput"
+for c in 9 90 900 9000 90000 900000 9000000; do
+    cat ~/karma-eval/$config-c$c.results | grep -i "throughput" | awk '{sum += $5} END {print sum;}'
+done
+
+echo "Fairness"
+for c in 9 90 900 9000 90000 900000 9000000; do
+    min_alloc=$(cat ~/karma-eval/$config-c$c.results | grep -i "total useful allocation" | awk '{print $7;}' | sort -n | head -n 1)
+    max_alloc=$(cat ~/karma-eval/$config-c$c.results | grep -i "total useful allocation" | awk '{print $7;}' | sort -n | tail -n 1)
+    awk -v min_alloc=$min_alloc -v max_alloc=$max_alloc 'BEGIN {print min_alloc/max_alloc}'
+done
+
+
