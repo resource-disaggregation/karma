7757c819b631d8a5ebf4c8bb138c411b2fa67401
Midhul <midhul.v@gmail.com>
2022-03-31 02:21:18 -0400

    guaranteed share to start_drivers4

--- sbin/collect_varyg.sh
+++ sbin/collect_varyg.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# NOTE: Hard coded for 100 users
+
 sbin="`dirname "$0"`"
 sbin="`cd "$sbin"; pwd`"
 
@@ -22,6 +24,13 @@ for g in 8 6 4 2 0; do
     min_alloc=$(cat ~/karma-eval/$config-g$g.results | grep -i "total useful allocation" | awk '{print $7;}' | sort -n | head -n 1)
     max_alloc=$(cat ~/karma-eval/$config-g$g.results | grep -i "total useful allocation" | awk '{print $7;}' | sort -n | tail -n 1)
     awk -v min_alloc=$min_alloc -v max_alloc=$max_alloc 'BEGIN {print min_alloc/max_alloc}'
+done
+
+echo "Disparity (throughput)"
+for g in 8 6 4 2 0; do
+    median_xput=$(cat ~/karma-eval/$config-g$g.results | grep -i "throughput" | awk '{print $5;}' | sort -n | head -n 50 | tail -n 1)
+    min_xput=$(cat ~/karma-eval/$config-g$g.results | grep -i "throughput" | awk '{print $5;}' | sort -n | head -n 1)
+    awk -v min_xput=$min_xput -v median_xput=$median_xput 'BEGIN {print median_alloc/min_xput}'
 done
 
 
