75fcc397f124e04e3dbacf35e9197082e75497b0
Midhul <midhul.v@gmail.com>
2022-03-31 00:10:13 -0400

    guaranteed share to start_drivers4

--- sbin/start_drivers4.sh
+++ sbin/start_drivers4.sh
@@ -25,6 +25,7 @@ init_credits="${12}"
 mode="${13}"
 num_shards="${14}"
 shard_idx="${15}"
+guarantee="${16}"
 # tenant_ids=('92' '45' '14' '79' '93' '1' '54' '96' '44' '76')
 # tenant_ids=('0' '1' '2' '3' '4' '5' '6' '7' '8' '9' '10' '11' '12' '13' '14' '15' '16' '17' '18' '19' '20' '21' '22' '23' '24' '25' '26' '27' '28' '29' '30' '31' '32' '33' '34' '35' '36' '37' '38' '39' '40' '41' '42' '43' '44' '45' '46' '47' '48' '49' '50' '51' '52' '53' '54' '55' '56' '57' '58' '59' '60' '61' '62' '63' '64' '65' '66' '67' '68' '69' '70' '71' '72' '73' '74' '75' '76' '77' '78' '79' '80' '81' '82' '83' '84' '85' '86' '87' '88' '89' '90' '91' '92' '93' '94' '95' '96' '97' '98' '99')
 # alt_tenants=('50' '51' '52' '53' '54' '55' '56' '57' '58' '59' '60' '61' '62' '63' '64' '65' '66' '67' '68' '69' '70' '71' '72' '73' '74' '75' '76' '77' '78' '79' '80' '81' '82' '83' '84' '85' '86' '87' '88' '89' '90' '91' '92' '93' '94' '95' '96' '97' '98' '99')
@@ -32,7 +33,7 @@ shard_idx="${15}"
 
 rm -f ~/karma-eval/$config.tenant*
 
-python3 ~/jiffy/scripts/compute_allocations.py $config $trace_file $alloc $fair_share $init_credits 0 $fair_share 1 none 0 $alt_file $selfish_file
+python3 ~/jiffy/scripts/compute_allocations.py $config $trace_file $alloc $fair_share $init_credits 0 $guarantee 1 none 0 $alt_file $selfish_file
 echo "Allocations computed"
 
 if [ "$mode" != "norun" ]; then
