c89d9382f408c6470a2b1715931082ee7741a4e2
Midhul <midhul.v@gmail.com>
2022-03-31 02:09:14 -0400

    guaranteed share to start_drivers4

---
+++ sbin/collect_varyg.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+sbin="`dirname "$0"`"
+sbin="`cd "$sbin"; pwd`"
+
+config=$1
+total_blocks=$2
+duration=$3
+
+echo "Utilization"
+for g in 0 2 4 6 8; do
+    cat ~/karma-eval/$config-g$g.results | grep -i "total useful allocation" | awk -v blocks=$total_blocks -v dur=$duration '{sum += $7} END {print sum/(blocks*dur);}'
+done
+
+echo "Throughput"
+for g in 0 2 4 6 8; do
+    cat ~/karma-eval/$config-g$g.results | grep -i "throughput" | awk '{sum += $5} END {print sum;}'
+done
+
+echo "Fairness"
+for g in 0 2 4 6 8; do
+    min_alloc=$(cat ~/karma-eval/$config-g$g.results | grep -i "total useful allocation" | awk '{print $7;}' | sort -n | head -n 1)
+    max_alloc=$(cat ~/karma-eval/$config-g$g.results | grep -i "total useful allocation" | awk '{print $7;}' | sort -n | tail -n 1)
+    awk -v min_alloc=$min_alloc -v max_alloc=$max_alloc 'BEGIN {print min_alloc/max_alloc}'
+done
+
+
